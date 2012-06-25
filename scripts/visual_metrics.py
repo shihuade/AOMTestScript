42ed7f6f2feda41bea71ca4f82043620070206a1
Lou Quillio <louquillio@google.com>
2012-06-25 12:25:19 -0700

    HTML5 output for visualizer page and docs (vs. HTML4 Trans.)
    Change-Id: Ie7cc0e1519a7109f127633f2cb56dcdccca3fa77
    modified:   scripts/visual_metrics.py
    modified:   template.html

--- scripts/visual_metrics.py
+++ scripts/visual_metrics.py
@@ -108,11 +108,10 @@ def HandleFiles(variables):
   file_pattern = variables[1]
   baseline_dir = variables[2]
   print """
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-  "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html>
 <html lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta charset="utf-8">
 <title>VP8 Results</title>
 <style type="text/css">
 body {
